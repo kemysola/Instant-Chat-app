e49db5bffd93dfee749a719bf61e5a7ffc7a615d
Adekemi Awomolo <dakemilola@gmail.com>
2021-08-10 04:34:37 +0100

    First commit

---
+++ chat.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+	console.log("ready")
+  //$("p").mouseover(function(){
+    //$(this).hide(2000);
+ 
+  //});
+
+  $("#bint").click(function(){
+  	$("#ppp").fadeToggle(1000);[]
+  	console.log('toggle');
+
+  })
+  $(".jil").hover(function(){
+  $(this).fadeTo(4000);
+})
+});
